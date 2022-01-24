a5e4ab9ed529eac0badcf744a39834e92c19cd13
Sam Ageloff <samageloff@stockx.com>
2022-01-24 12:46:18 -0500

    removing postgres url remote

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env(
-    "postgres://ydszpforfgslxo:cb86c909d2529d9ed87d0bb18750197ac4fb30a23dacb696cf46e92089605737@ec2-44-199-52-133.compute-1.amazonaws.com:5432/d89ad1v1d46r1r"
-  ),
+  url: env("DATABASE_URL"),
 });
