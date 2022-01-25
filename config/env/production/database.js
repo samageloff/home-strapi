af0ecc18b69e2ebd820f514f742b350d34c051cb
Sam Ageloff <samageloff@stockx.com>
2022-01-25 11:53:36 -0500

    reverting some stuff

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,18 +5,19 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
       database: config.database,
       user: config.user,
       password: config.password,
+      port: config.port,
+      host: config.host,
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        rejectUnauthorized: false,
       },
-    },
-    options: {
-      ssl: env.bool("DATABASE_SSL", false),
     },
     debug: false,
   },
 });
+
+/postgres:
+//ydszpforfgslxo:
+cb86c909d2529d9ed87d0bb18750197ac4fb30a23dacb696cf46e92089605737@ec2-44-199-52-133.compute-1.amazonaws.com:5432/d89ad1v1d46r1r/admin/:path
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,3 @@
 module.exports = ({ env }) => ({
   url: env("DATABASE_URL"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: env("JWT_SECRET"),
-    },
-  },
-  host: env("HOST") || "0.0.0.0",
 });
